a86162b306c989bbcf58c8052b0cab7b2ab6ea0e
Sherzo <sherzo.florez@gmail.com>
2023-04-28 09:42:10 -0400

    📦 NEW: Reinicar y salir del juego

--- src/pages/GameOver.tsx
+++ src/pages/GameOver.tsx
@@ -1,18 +1,28 @@
+import { useNavigate } from 'react-router-dom'
 import { Button } from '../ui/Button'
 import { Subtitle } from '../ui/Subtitle'
 import { Title } from '../ui/Title'
 
 export const GameOver = (): JSX.Element => {
+  const navigate = useNavigate()
+
+  const restart = () => {
+    navigate('/game')
+  }
+  const exit = () => {
+    navigate('/')
+  }
+
   return (
     <div className="flex flex-col justify-center items-center min-h-screen lg:w-1/2 mx-auto">
-      <Title>Your Win!</Title>
+      <Title>Your Win 🎉!</Title>
       <Subtitle>
-        Congrants! You are complete the game in{' '}
-        <span className="font-bold">12:30s</span>
+        Congrants Saul! You are complete the game in{' '}
+        <span className="font-bold">⏱️ 12:30s</span>
       </Subtitle>
       <div className="flex mt-4 gap-4 justify-center items-center">
-        <Button>Restart</Button>
-        <Button>Exit</Button>
+        <Button onClick={restart}>Restart 💪</Button>
+        <Button onClick={exit}>Exit 🖐️</Button>
       </div>
     </div>
   )
