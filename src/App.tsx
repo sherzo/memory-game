c9850faef84bbb672ef9dd385a29774927e50897
Sherzo <sherzo.florez@gmail.com>
2023-04-28 11:21:21 -0400

    📦 NEW: Agregando user provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
+import { UserProvider } from './context/user-context'
 import { router } from './router'
 
 function App() {
   return (
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <UserProvider>
+        <RouterProvider router={router} />
+      </UserProvider>
     </React.StrictMode>
   )
 }
