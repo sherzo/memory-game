0e639991fd667ea3a53ead9994cbb85110d83b3a
Sherzo <sherzo.florez@gmail.com>
2023-04-28 09:40:51 -0400

    📦 NEW: Estilos

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -5,7 +5,7 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 export const Button = ({ children, ...rest }: ButtonProps) => {
   return (
     <button
-      className="block bg-gray-800 text-white rounded-md p-4 min-w-full text-xl"
+      className="block bg-gray-800 text-white rounded-md p-4 min-w-full text-xl disabled:opacity-75 disabled:cursor-not-allowed"
       {...rest}
     >
       {children}
