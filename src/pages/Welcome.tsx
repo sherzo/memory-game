e3d0e30ad0a6463010e4cabd3741468da4670bdc
Sherzo <sherzo.florez@gmail.com>
2023-04-28 09:41:53 -0400

    📦 NEW: Detalles de estilos

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -21,18 +21,18 @@ export const Welcome = (): JSX.Element => {
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen lg:w-1/2 mx-auto">
-      <Title>Memory Game</Title>
+      <Title>Memory Game 🎮</Title>
       <Subtitle>Test your concentration in a fun way</Subtitle>
       <form action="#" className="mt-4 min-w-full" onSubmit={handleSubmit}>
         <Input
           name="full_name"
           onChange={handleOnChange}
           value={name}
-          placeholder="What is your name"
+          placeholder="What is your name?"
           id="full_name"
-          className="border px-4 py-4 rounded-md text-center min-w-full mb-2 border-gray-500 text-xl"
+          className="border  px-4 py-4 rounded-md text-center min-w-full mb-2 border-gray-500 text-xl"
         />
-        <Button>Play!</Button>
+        <Button disabled={name.length < 3}>Play 🕹️</Button>
       </form>
     </div>
   )
