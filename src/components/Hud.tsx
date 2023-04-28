e09c224d4cf465a3be074c6d9da58e001af320db
Sherzo <sherzo.florez@gmail.com>
2023-04-28 15:57:32 -0400

    🐛 FIX: Hood responsive

--- src/components/Hud.tsx
+++ src/components/Hud.tsx
@@ -10,14 +10,14 @@ export const Hud = ({
   return (
     <div
       className="flex bg-white mx-auto justify-between 
-      sm:justify-around shadow-lg rounded-md mt-6 px-6 py-2 
+      sm:justify-around shadow-lg rounded-md mt-6 px-1 lg:px-6 py-2 
       lg:w-10/12 mb-10"
     >
-      <span className="font-bold text-2xl ">
+      <span className="font-bold text-xl lg:text-2xl">
         ✅ Hits: <span className="font-black">{score}</span>
       </span>
       <StopWatch />
-      <span className="font-bold text-2xl ">
+      <span className="font-bold text-xl lg:text-2xl">
         💥 Misses: <span className="font-black">{misses}</span>
       </span>
     </div>
--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -19,12 +19,12 @@ export const StopWatch = () => {
 
   return (
     <div
-      className="bg-blue-900 h-16 w-24 rounded-md sm:w-40
+      className="bg-blue-900 h-12 sm:h-16 w-24 rounded-md sm:w-40
           shadow-lg absolute left-1/2 transform -translate-x-1/2 
           -translate-y-1/2 mt-4 flex justify-center items-center 
         "
     >
-      <span className="text-2xl text-white font-bold">
+      <span className="text-xl lg:text-2xl text-white font-bold">
         <Timer time={time} />
       </span>
     </div>
